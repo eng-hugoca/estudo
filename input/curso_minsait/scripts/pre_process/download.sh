8c4fd10ee01a1bd6f197d158c1885c304ee0a502
Hugo Amorim <eng.hugoca@gmail.com>
2023-05-20 14:53:06 +0000

    v1

--- input/curso_minsait/scripts/pre_process/download.sh
+++ input/curso_minsait/scripts/pre_process/download.sh
@@ -1,8 +1,18 @@
 # !/bin/bash
 
-CATEGORIA="categoria"
+CATEGORIA="categoria cidade cliente"
 
-cd ../../raw
-mkdir $CATEGORIA
-cd $CATEGORIA
-curl -O https://raw.githubusercontent.com/caiuafranca/dados_curso/main/categoria.csv
+
+for NOME in $CATEGORIA
+do
+    cd /workspace/estudo/input/curso_minsait/raw/
+    mkdir $NOME
+    cd $NOME
+    curl -O https://raw.githubusercontent.com/caiuafranca/dados_curso/main/$NOME.csv
+done
+
+#teste="uni duni"
+#for i in $teste
+#do
+#    echo $i
+#done
